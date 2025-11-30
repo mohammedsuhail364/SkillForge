dbe3a35ba0bfb0068bd50cb0613eb021465c0465
mohammedsuhail364 <mohammedthanish364@gmail.com>
2025-11-30 15:25:38 +0530

    refactoring the sessionController.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -108,9 +108,19 @@ export async function joinSession(req, res) {
     const clerkId = req.user.clerkId;
     const session = await Session.findById(id);
     if (!session) return res.status(404).json({ message: "session not fount" });
+    if (session.status !== "active") {
+      return res.status(400).json({
+        message: "Cannot join a completed session",
+      });
+    }
+    if (session.host.toString() === userId.toString()) {
+      return res.status(400).json({
+        message: "Host cannot join their own session as participant",
+      });
+    }
     // check if session is already full - has a participant
     if (session.participant)
-      return res.status(404).json({ message: "Session is full" });
+      return res.status(409).json({ message: "Session is full" }); // 409 -> conflict
     session.participant = userId;
     await session.save();
     const channel = chatClient.channel("messaging", session.callId);
@@ -141,22 +151,23 @@ export async function endSession(req, res) {
         message: "Session is already completed",
       });
     }
-    session.status = "completed";
+
     // delete stream video call
     const call = streamClient.video.call("default", session.callId);
     await call.delete({ hard: true });
     // delete stream chat channel
     const channel = chatClient.channel("messaging", session.callId);
     await channel.delete();
+    session.status = "completed";
     session.save();
     res.status(200).json({
       session,
       message: "Session ended successfully",
     });
   } catch (error) {
-    console.log("Error in endSession controller: ",error.message);
+    console.log("Error in endSession controller: ", error.message);
     res.status(500).json({
-      message:"Internal Server Error"
-    })
+      message: "Internal Server Error",
+    });
   }
 }
