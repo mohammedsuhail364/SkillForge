03d4a00277a8477ce5a8440b6e046f06c5770e35
mohammedsuhail364 <mohammedthanish364@gmail.com>
2026-01-15 08:22:24 +0530

    dashboard page added

--- frontend/src/pages/DashBoardPage.jsx
+++ frontend/src/pages/DashBoardPage.jsx
@@ -1,8 +1,83 @@
+import { useNavigate } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import { useState } from "react";
+import {
+  useActiveSessions,
+  useCreateSession,
+  useMyRecentSessions,
+} from "../hooks/useSessions";
+import NavBar from "../Components/NavBar";
+import WelcomeSection from "../Components/WelcomeSection";
+import StatCards from "../Components/StatCards";
+import ActiveSessions from "../Components/ActiveSessions";
+import RecentSessions from "../Components/RecentSessions";
+import CreateSessionModal from "../Components/CreateSessionModal";
 
 function DashBoardPage() {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const [showCreateModel, setShowCreateModel] = useState(false);
+  const [roomConfig, setRoomConfig] = useState({ problem: "", difficulty: "" });
+  const createSessionMutation = useCreateSession();
+  const { data: activeSessionsData, isLoading: loadingActiveSessions } =
+    useActiveSessions();
+  const { data: recentSessionsData, isLoading: loadingRecentSessions } =
+    useMyRecentSessions();
+  const activeSessions = activeSessionsData?.sessions || [];
+  const recentSessions = recentSessionsData?.sessions || [];
+  const handleCreateRoom = () => {
+    if (!roomConfig.problem || !roomConfig.difficulty) return;
+    createSessionMutation.mutate(
+      {
+        problem: roomConfig.problem,
+        difficulty: roomConfig.difficulty.toLowerCase(),
+      },
+      {
+        onSuccess: (data) => {
+          setShowCreateModel(false);
+          navigate(`/session/${data.session._id}`);
+        },
+      }
+    );
+  };
+  const isUserInSession = (session) => {
+    if (!user.id) return false;
+    return (
+      session.host?.clerkId === user.id ||
+      session.participant?.clerkId === user.id
+    );
+  };
   return (
-    <div>DashBoardPage</div>
-  )
+    <>
+      <div className=" min-h-screen bg-base-300">
+        <NavBar />
+        <WelcomeSection onCreateSession={() => setShowCreateModel(true)} />
+        {/* Grid layout */}
+        <div className=" container mx-auto px-6 pb-16">
+          <div className=" grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <StatCards
+              activeSessionsCount={activeSessions.length}
+              recentSessionsCount={recentSessions.length}
+            />
+            <ActiveSessions
+            sessions={activeSessions}
+            isLoading={loadingActiveSessions}
+            isUserInSession={isUserInSession}
+            />
+          </div>
+          <RecentSessions sessions={recentSessions} isLoading={loadingRecentSessions}/>
+        </div>
+      </div>
+      <CreateSessionModal
+        isOpen={showCreateModel}
+        onClose={() => setShowCreateModel(false)}
+        roomConfig={roomConfig}
+        setRoomConfig={setRoomConfig}
+        onCreateRoom={handleCreateRoom}
+        isCreating={createSessionMutation.isPending}
+      />
+    </>
+  );
 }
 
-export default DashBoardPage
+export default DashBoardPage;
